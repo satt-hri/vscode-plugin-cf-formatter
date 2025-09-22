71c2c20174184411e1ca9984c6c291b390df948b
h_ri <h_ri@satt.jp>
2025-09-22 18:43:02 +0900

    AST作成 Fixes #19

---
+++ src/core/AST.ts
@@ -0,0 +1,90 @@
+import * as vscode from "vscode";
+import { FormatState } from "./FormatState";
+
+class ASTNode {
+	private children: ASTNode[] = [];
+
+	constructor(
+		public readonly name: string,
+		public readonly type: string,
+		public range?: vscode.Range,
+		public context?: string
+	) {}
+
+	// 添加子节点
+	addChildren(node: ASTNode) {
+		this.children.push(node);
+	}
+
+	// 删除指定索引的子节点
+	removeChildren(i: number) {
+		if (i >= 0 && i < this.children.length) {
+			this.children.splice(i, 1);
+		}
+	}
+
+	// 获取所有子节点（只读）
+	getChildren(): readonly ASTNode[] {
+		return this.children;
+	}
+}
+
+class CFCNode extends ASTNode {
+	constructor() {
+		super("CFC", "cfcomponent");
+	}
+}
+
+export function creatAst(
+	line: vscode.TextLine,
+	lineIndex: number,
+	edits: vscode.TextEdit[],
+	state: FormatState,
+	document: vscode.TextDocument
+): boolean {
+	const root = new CFCNode();
+	const cfTagRegex = /<\s*(cf\w+)\b[\s\S]*?(\/?)>/gi;
+	let currentTag = root;
+
+	let stack: ASTNode[] = [root];
+	for (let i = 0; i < document.lineCount; i++) {
+		const originalText = document.lineAt(i).text;
+		const trimText = originalText.trim();
+		if (!trimText) continue;
+		const matches = [...trimText.matchAll(cfTagRegex)];
+		let lastIndex = 0;
+		if (matches.length) {
+			matches.forEach((match) => {
+				console.log(match);
+
+				// 1. 取标签前面的“非标签内容”
+				if (match.index > lastIndex) {
+					const before = trimText.slice(0, match.index);
+					console.log("前面非标签:", before);
+
+					currentTag.context = currentTag.context + before;
+				}
+				// 2. 更新 lastIndex
+				lastIndex = match.index + match[0].length;
+
+				const fullText = match[0];
+				const tagName = match[1];
+				const tagNode = new ASTNode(tagName, "cf-tag");
+				tagNode.context = tagNode.context + fullText;
+				currentTag.addChildren(tagNode);
+				currentTag = tagNode;
+
+				// 3. 最后可能还有剩余“非标签内容”
+				if (lastIndex < trimText.length) {
+					const after = trimText.slice(lastIndex);
+					console.log("后面非标签:", after);
+					currentTag.context = currentTag.context + after;
+				}
+			});
+		} else {
+			currentTag.context = currentTag.context + trimText;
+		}
+	}
+
+	return true;
+}
